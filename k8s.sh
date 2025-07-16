30d34425ebd22a906306fde0ed8c72013a566913
Adrian Paleacu <adrian.paleacu@gmail.com>
2025-07-16 13:21:42 +0300

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
+
+CLUSTER_NAME=$(hostname)
+IP_ADDR=$(ip route get 1 | awk '{print $7; exit}')
+
 cat <<EOF > ~/cluster.yaml
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 networking:
-  apiServerAddress: "192.168.0.92"
+  apiServerAddress: "$IP_ADDR"
   apiServerPort: 6444
 nodes:
   - role: control-plane
@@ -13,4 +17,4 @@ EOF
 sudo sysctl fs.inotify.max_user_watches=1955360
 sudo sysctl fs.inotify.max_user_instances=4280
 
-kind create cluster --config ~/cluster.yaml
+kind create cluster --name "$CLUSTER_NAME" --config ~/cluster.yaml
